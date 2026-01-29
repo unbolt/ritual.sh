021f5fc304d25bfb3c68757470556600bdf7fbb4
Dan <dan@unbo.lt>
2026-01-29 13:30:08 +0000

    Fixing  target score

--- static/games/clicker/index.service.worker.js
+++ static/games/clicker/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1769693184|4645131895';
+const CACHE_VERSION = '1769693384|4844716121';
 /** @type {string} */
 const CACHE_PREFIX = 'Clicker-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
